eb86166f8e1b95ba58b332188607894f317f30ea
Ben Stucke <ben-stucke@hotmail.de>
2019-07-24 14:45:34 +0200

    jar arg can be left empty now if db is in localhost:3306/erp_base

--- src/main/java/logic/Main.java
+++ src/main/java/logic/Main.java
@@ -10,8 +10,14 @@ public class Main {
     public static void main(String[] args) {
 
         //as arg you need to pass the db uri where your holiday collection is located like: "localhost:3306/erp_base?user=root&password="
+        String dbUri;
         int year = 2019;
         List<Holiday> holidays=NewYearService.getHolidays(year, States.HE);
-        NewYearService.writeHolidaysToDB(holidays, args[0]);
+        if(args.length ==0) {
+            dbUri="localhost:3306/erp_base?user=root&password=";
+        } else {
+            dbUri=args[0];
+        }
+        NewYearService.writeHolidaysToDB(holidays, dbUri);
     }
 }
